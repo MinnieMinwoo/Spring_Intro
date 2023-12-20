096378a649d87a7eaf0de34dc28e14eb3fb5d45d
SnowCat <nhs075241@gmail.com>
2023-12-20 15:25:08 +0900

    Lec1: Add development setting

---
+++ spring_practice/src/main/java/com/practice/mollu/HelloController.java
@@ -0,0 +1,15 @@
+package com.practice.mollu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+  @GetMapping("/hello")
+  @ResponseBody
+  public String hello() {
+    return "몰루";
+  }
+}
---
+++ spring_practice/src/main/java/com/practice/mollu/MolluApplication.java
@@ -0,0 +1,13 @@
+package com.practice.mollu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MolluApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MolluApplication.class, args);
+	}
+
+}
