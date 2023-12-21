a2d299f7edf73f85a44759fe0d6b6fbf13b402a8
SnowCat <nhs075241@gmail.com>
2023-12-21 16:48:39 +0900

    2-5: JPA, Entity, Repository

---
+++ spring_practice/src/main/java/com/practice/mollu/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.practice.mollu;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ spring_practice/src/main/java/com/practice/mollu/MainController.java
@@ -0,0 +1,15 @@
+package com.practice.mollu;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+  @GetMapping("/mollu")
+  @ResponseBody
+  public String index() {
+    return "몰?루";
+  }
+}
